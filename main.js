0fff919c876426e35d46e389b06f49be81de8a79
MohamedMoustir <itsmoustir@gmail.com>
2024-10-20 23:31:53 +0100

    add file js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 
-// let img = document.querySelector("#img")
-//  let blurr=document.querySelector("#blur")
-//  console.log(blurr.value)
+let img = document.querySelector("#img")
+let upload = document.getElementById("upload")
+
+
+upload.onchange = function(){
+    let file =  new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    
+    file.onload=function(){
+        img.src=file.result;
+    }
+}
+
+ 
+
+
 
 
 
