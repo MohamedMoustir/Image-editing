6ab6afbe84f7668c0f544fa7c112748a0df56844
MohamedMoustir <itsmoustir@gmail.com>
2024-10-21 00:36:17 +0100

    add tous les filters in js

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 
 let img = document.querySelector("#img")
 let upload = document.getElementById("upload")
-let blurr = document.getElementById("blur"); 
-let brightness = document.getElementById("brightness"); 
-let contrast = document.getElementById("contrast"); 
-let grayscale = document.getElementById("grayscale"); 
-let sepia = document.getElementById("sepia"); 
-let drop_shadow = document.getElementById("drop-shadow"); 
+let blurr = document.getElementById("blur");
+let brightness = document.getElementById("brightness");
+let contrast = document.getElementById("contrast");
+let grayscale = document.getElementById("grayscale");
+let sepia = document.getElementById("sepia");
 
-upload.onchange = function(){
-    let file =  new FileReader();
+upload.onchange = function () {
+    let file = new FileReader();
     file.readAsDataURL(upload.files[0]);
-    
-    file.onload=function(){
-        img.src=file.result;
+
+    file.onload = function () {
+        img.src = file.result;
     }
 }
 
- let inputs = document.querySelectorAll(".Edite input")
+let inputs = document.querySelectorAll(".Edite input")
 
-inputs.forEach( (item)=>{
-    item.addEventListener("input",function(){
-        img.style.filter=`
+inputs.forEach((item) => {
+    item.addEventListener('input', function () {
+        img.style.filter = `
         blur(${blurr.value}px)
-
+        brightness(${brightness.value}%)
+        contrast(${contrast.value}%)
+        grayscale(${grayscale.value})
+        sepia(${sepia.value}%)
+             
       `
     })
 
-} )
+})
 
 
 
