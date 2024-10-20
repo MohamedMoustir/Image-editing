31ee61b640168ef1cabe386121d15cb9f37d9fac
MohamedMoustir <itsmoustir@gmail.com>
2024-10-21 00:12:44 +0100

    add nwe filter in file js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 let img = document.querySelector("#img")
 let upload = document.getElementById("upload")
 let blurr = document.getElementById("blur"); 
+let brightness = document.getElementById("brightness"); 
+let contrast = document.getElementById("contrast"); 
+let grayscale = document.getElementById("grayscale"); 
+let sepia = document.getElementById("sepia"); 
+let drop_shadow = document.getElementById("drop-shadow"); 
 
 upload.onchange = function(){
     let file =  new FileReader();
@@ -18,7 +23,7 @@ inputs.forEach( (item)=>{
     item.addEventListener("input",function(){
         img.style.filter=`
         blur(${blurr.value}px)
-        
+
       `
     })
 
